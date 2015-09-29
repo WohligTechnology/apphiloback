a2068fc72df18e3424be6c93b42f1182e3c2b843
Pooja <pthakare33@gmail.com>
2015-09-29 10:24:45 +0530

    ui proper

--- application/views/backend/vieweventvideo.php
+++ application/views/backend/vieweventvideo.php
@@ -30,7 +30,7 @@
 </div>
 <script>
     function drawtable(resultrow) {
-           return "<tr><td>" + resultrow.id + "</td><td>" + resultrow.event + "</td><td>" + resultrow.url + "</td><td>" + resultrow.status + "</td><td>" + resultrow.order + "</td><td><a class='btn btn-primary btn-xs' href='<?php echo site_url('site/editeventvideo?id=');?>" + resultrow.id + "&eventid=" + resultrow.eventid + "'><i class='material-icons propericon'>system_update_alt</i></a><a class='btn btn-danger btn-xs' onclick=return confirm(\"Are you sure you want to delete?\") href='<?php echo site_url('site/deleteeventvideo?id='); ?>" + resultrow.id + "&eventid=" + resultrow.eventid + "'><i class='material-icons propericon'>delete</i></a></td></tr>";
+           return "<tr><td>" + resultrow.id + "</td><td>" + resultrow.event + "</td><td>" + resultrow.url + "</td><td>" + resultrow.status + "</td><td>" + resultrow.order + "</td><td><a class='btn btn-primary btn-xs' href='<?php echo site_url('site/editeventvideo?id=');?>" + resultrow.id + "&eventid=" + resultrow.eventid + "'><i class='material-icons propericon'>system_update_alt</i></a><a class='btn btn-danger btn-xs' onclick=\"return confirm('Are you sure you want to delete?');\" href='<?php echo site_url('site/deleteeventvideo?id='); ?>" + resultrow.id + "&eventid=" + resultrow.eventid + "'><i class='material-icons propericon'>delete</i></a></td></tr>";
     }
     generatejquery('<?php echo $base_url;?>');
 </script>
