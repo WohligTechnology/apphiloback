811ae7b6f529c4985726ccffece9b17814a2f815
mahesh maurya <mahesh@wohlig.com>
2015-10-05 19:30:09 +0530

    sum changes

--- application/views/backend/createevents.php
+++ application/views/backend/createevents.php
@@ -17,7 +17,7 @@
             </div>
             <div class="row">
                 <div class="file-field input-field col s12">
-                    <div class="btn grey darken-1">
+                    <div class="btn blue darken-4">
                         <span>Image</span>
                         <input name="image" type="file" multiple>
                     </div>
@@ -27,10 +27,13 @@
                 </div>
             </div>
             <div class="row">
-            <textarea id="some-textarea" name="content" placeholder="Enter text ..."><?php echo set_value( 'content');?></textarea>
+            <div class="col s12">
+                 <textarea id="some-textarea" name="content" placeholder="Enter text ..."><?php echo set_value( 'content');?></textarea>
             <script type="text/javascript">
                 $('#some-textarea').wysihtml5();
             </script>
+            </div>
+           
         </div> 
             <div class="row">
                 <div class="input-field col s6">
@@ -50,13 +53,18 @@
                      <label>starttime</label>
                 </div>
             </div>
-            <div class=" form-group">
+            <div class="row">
+                <div class="col s6">
+                         <div class=" form-group">
                 <label class="col-sm-2 control-label">&nbsp;</label>
                 <div class="col-sm-4">
-                    <button type="submit" class="btn btn-primary waves-effect waves-light green">Save</button>
+                    <button type="submit" class="btn btn-primary waves-effect waves-light blue darken-4">Save</button>
                     <a href="<?php echo site_url('site/viewevents'); ?>" class="btn btn-secondary waves-effect waves-light red">Cancel</a>
                 </div>
             </div>
+                </div>
+            </div>
+       
         </form>
 </div>
 
--- application/views/backend/createnotification.php
+++ application/views/backend/createnotification.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col s12">
-        <h4 class="pad-left-15">Create Notification</h4>
+        <h4 class="pad-left-15">Create Notifications</h4>
     </div>
     <form class="col s12" method="post" action="<?php echo site_url('site/createnotificationsubmit');?>" enctype="multipart/form-data">
         <div class="row">
@@ -70,7 +70,7 @@
 
         <div class="row">
             <div class="file-field input-field col s12">
-                <div class="btn brown">
+                <div class="btn blue darken-4">
                     <span>Image</span>
                     <input name="image" type="file" multiple>
                 </div>
@@ -85,13 +85,18 @@
                 <label for="content">Content</label>
             </div>
         </div>
-        <div class=" form-group">
+        <div class="row">
+            <div class="col s6">
+                    <div class=" form-group">
             <label class="col-sm-2 control-label">&nbsp;</label>
             <div class="col-sm-4">
-                <button type="submit" class="btn btn-primary waves-effect waves-light green">Save</button>
+                <button type="submit" class="btn btn-primary waves-effect waves-light blue darken-4">Save</button>
                 <a href="<?php echo site_url('site/viewnotification'); ?>" class="btn btn-secondary waves-effect waves-light red">Cancel</a>
             </div>
         </div>
+            </div>
+        </div>
+    
     </form>
 </div>
 <script type="text/javascript">
