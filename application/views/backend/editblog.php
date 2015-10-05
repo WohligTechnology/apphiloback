53d7bbc5310c950eb1d0b08633993854749b9115
mahesh maurya <mahesh@wohlig.com>
2015-10-05 18:54:44 +0530

    edit blog alighnment done

--- application/views/backend/editblog.php
+++ application/views/backend/editblog.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col s12">
-    <h4 class="pad-left-15">Edit Blog</h4>
+    <h4 class="pad-left-15">Edit Blogs</h4>
     </div>
 </div>
 <div class="row">
@@ -20,11 +20,15 @@
             </div>
         </div>
                <div class="row">
+                 <div class="col s12">
+                     
+               
                   <textarea id="some-textarea" name="content" placeholder="Enter text ..."><?php echo set_value( 'content',$before->content);?></textarea>
             <script type="text/javascript">
                 $('#some-textarea').wysihtml5();
             </script>
             </div>
+              </div>
              <div class="row">
             <div class="file-field input-field col s12">
                 <div class="btn brown">
@@ -51,13 +55,17 @@
                 <input type="text" name="timestamp" value="<?php echo set_value('timestamp',$before->timestamp);?>">
             </div>
         </div>
-
-        <div class=" form-group">
+<div class="row">
+    <div class="col s6">
+           <div class=" form-group">
             <label class="col-sm-2 control-label">&nbsp;</label>
             <div class="col-sm-4">
                 <button type="submit" class="btn btn-primary jsonsubmit waves-effect waves-light green">Save</button>
                 <a href="<?php echo site_url('site/viewblog'); ?>" class="btn btn-secondary waves-effect waves-light red">Cancel</a>
             </div>
         </div>
+    </div>
+</div>
+     
     </form>
 </div>
