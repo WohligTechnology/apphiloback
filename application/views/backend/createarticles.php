2ad5eab3af1c0d80f24fb9e46a6b855781e34529
mahesh maurya <mahesh@wohlig.com>
2015-10-05 19:35:56 +0530

    done

--- application/views/backend/createarticles.php
+++ application/views/backend/createarticles.php
@@ -17,14 +17,17 @@
             </div>
         </div>
         <div class="row">
-            <textarea id="some-textarea" name="content" placeholder="Enter text ..."><?php echo set_value( 'content');?></textarea>
+           <div class="col s12">
+               <textarea id="some-textarea" name="content" placeholder="Enter text ..."><?php echo set_value( 'content');?></textarea>
             <script type="text/javascript">
                 $('#some-textarea').wysihtml5();
             </script>
+           </div>
+            
         </div>
         <div class="row">
             <div class="file-field input-field col s12">
-                <div class="btn brown">
+                <div class="btn blue darken-4">
                     <span>Image</span>
                     <input name="image" type="file" multiple>
                 </div>
@@ -33,12 +36,17 @@
                 </div>
             </div>
         </div>
-        <div class=" form-group">
+        <div class="row">
+            <div class="col s6">
+                    <div class=" form-group">
             <label class="col-sm-2 control-label">&nbsp;</label>
             <div class="col-sm-4">
-                <button type="submit" class="btn btn-primary waves-effect waves-light green">Save</button>
+                <button type="submit" class="btn btn-primary waves-effect waves-light blue darken-4">Save</button>
                 <a href="<?php echo site_url('site/viewarticles'); ?>" class="btn btn-secondary waves-effect waves-light  red">Cancel</a>
             </div>
         </div>
+            </div>
+        </div>
+    
     </form>
 </div>
