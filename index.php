82e95e276ca78e54684dd45a9876fcd39360124a
Chintan <chintan@wohlig.com>
2015-10-06 10:51:52 +0530

    Testing

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ if (defined('ENVIRONMENT'))
  * This variable must contain the name of your "system" folder.
  * Include the path if the folder is not in the same  directory
  * as this file.
- *
+ * Just Testing Tower
  */
 	$system_path = 'system';
 
