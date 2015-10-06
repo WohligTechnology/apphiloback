dace7b6a97c067b5a2baa802f36693ee0daaf7bb
Chintan Shah <chintan@wohlig.com>
2015-10-06 10:43:30 +0530

    changes

--- application/views/backend/block/eventblock.php
+++ application/views/backend/block/eventblock.php
@@ -2,8 +2,8 @@
     <div class="panel-body">
         <ul id="nav-mobile">
             <li><a class="waves-effect waves-light <?php if($this->uri->segment(2)=="editevents") { echo "active"; } ?>" href="<?php echo site_url('site/editevents?id=').$before1; ?>">Event Details</a></li>
-            <li><a class="waves-effect waves-light <?php if($this->uri->segment(2)=="vieweventvideo") { echo "active"; } ?>" href="<?php echo site_url('site/vieweventvideo?id=').$before2; ?>">Event Video</a></li>
-            <li><a class="waves-effect waves-light <?php if($this->uri->segment(2)=="vieweventimages") { echo "active"; } ?>" href="<?php echo site_url('site/vieweventimages?id=').$before3; ?>">Event Images</a></li>
+            <li><a class="waves-effect waves-light <?php if($this->uri->segment(2)=="vieweventvideo" || $this->uri->segment(2)=="createeventvideo" || $this->uri->segment(2)=="editeventvideo") { echo "active"; } ?>" href="<?php echo site_url('site/vieweventvideo?id=').$before2; ?>">Event Video</a></li>
+            <li><a class="waves-effect waves-light <?php if($this->uri->segment(2)=="vieweventimages" || $this->uri->segment(2)=="editeventimages"  || $this->uri->segment(2)=="createeventimages" ) { echo "active"; } ?>" href="<?php echo site_url('site/vieweventimages?id=').$before3; ?>">Event Images</a></li>
         </ul>
     </div>
 </section>
--- application/views/backend/block/videoblock.php
+++ application/views/backend/block/videoblock.php
@@ -2,7 +2,7 @@
     <div class="panel-body">
         <ul id="nav-mobile">
             <li><a class="waves-effect waves-light <?php if($this->uri->segment(2)=="editvideogallery") { echo "active"; } ?>" href="<?php echo site_url('site/editvideogallery?id=').$before1; ?>">Video Details</a></li>
-            <li><a class="waves-effect waves-light <?php if($this->uri->segment(2)=="viewvideogalleryvideo") { echo "active"; } ?>" href="<?php echo site_url('site/viewvideogalleryvideo?id=').$before2; ?>">Video Gallery Details</a></li>
+            <li><a class="waves-effect waves-light <?php if($this->uri->segment(2)=="viewvideogalleryvideo" || $this->uri->segment(2)=="editvideogalleryvideo" || $this->uri->segment(2)=="createvideogalleryvideo" ) { echo "active"; } ?>" href="<?php echo site_url('site/viewvideogalleryvideo?id=').$before2; ?>">Video Gallery Details</a></li>
         </ul>
     </div>
 </section>
