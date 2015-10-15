5d136591015192e40fc193410f89ec622a994e84
Chintan <chintan@wohlig.com>
2015-10-15 21:22:52 +0530

    change for create notification.

--- application/models/notification_model.php
+++ application/models/notification_model.php
@@ -7,6 +7,10 @@ public function create($linktype,$event,$video,$gallery,$article,$status,$blog,$
 {
 $query=$this->db->query("INSERT INTO `webapp_notification`(`status`, `event`, `video`, `link`, `image`,`linktype`,`blog`,`article`,`gallery`,`content`) VALUES (".$this->db->escape($status).",".$this->db->escape($event).",".$this->db->escape($video).",".$this->db->escape($link).",".$this->db->escape($image).",".$this->db->escape($linktype).",".$this->db->escape($blog).",".$this->db->escape($article).",".$this->db->escape($gallery).",".$this->db->escape($content).")");
 $id=$this->db->insert_id();
+
+$this->RestApi_model->sendNotificationAndroid($content,"",$image,$icon);
+$this->RestApi_model->sendNotificationIos($content);
+
 if(!$query)
 return  0;
 else
@@ -23,7 +27,7 @@ return $query;
 }
 public function edit($id,$linktype,$event,$video,$gallery,$article,$status,$blog,$link,$content,$image,$timestamp)
 {
-$query=$this->db->query("UPDATE `webapp_notification` 
+$query=$this->db->query("UPDATE `webapp_notification`
  SET `status` = ".$this->db->escape($status).", `event` = ".$this->db->escape($event).", `video` = ".$this->db->escape($video).",`image` = ".$this->db->escape($image).",`linktype` = ".$this->db->escape($linktype).",`link` = ".$this->db->escape($link).",`blog` = ".$this->db->escape($blog).",`article` = ".$this->db->escape($article).",`gallery` = ".$this->db->escape($gallery).",`content` = ".$this->db->escape($content).",`timestamp` = ".$this->db->escape($timestamp)."
  WHERE id = (".$this->db->escape($id).")");
 return 1;
