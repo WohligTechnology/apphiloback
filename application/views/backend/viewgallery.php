3c4ccb1d0a0f03e6e51dbd23a4672e2b032fbcc8
Pooja <pthakare33@gmail.com>
2015-10-17 14:05:30 +0530

    drag sort

--- application/views/backend/viewgallery.php
+++ application/views/backend/viewgallery.php
@@ -21,7 +21,7 @@
 </div>
 <script>
 function drawtable(resultrow) {
-         return "<li data-id='"+resultrow.id+"'><div class='card-panel'><tr><td>" + resultrow.id + "</td><td>" + resultrow.name + "</td><td>" + resultrow.status + "</td><td>" + resultrow.timestamp + "</td><td><a class='btn btn-primary btn-xs waves-effect waves-light blue darken-4 z-depth-0 less-pad' href='<?php echo site_url('site/editFrontMenu?id=');?>" + resultrow.id + "'><i class='material-icons'>mode_edit</i></a><a class='btn btn-danger btn-xs waves-effect waves-light red pad10 z-depth-0 less-pad' onclick=\"return confirm('Are you sure you want to delete?');\" href='<?php echo site_url('site/deleteFrontMenu?id='); ?>" + resultrow.id + "'><i class='material-icons propericon'>delete</i></a></td></tr></div><li>";
+         return "<li data-id='"+resultrow.id+"'><div class='card-panel'><tr><td>" + resultrow.id + "</td><td>" + resultrow.name + "</td><td>" + resultrow.status + "</td><td>" + resultrow.timestamp + "</td><td><a class='btn btn-primary btn-xs waves-effect waves-light blue darken-4 z-depth-0 less-pad' href='<?php echo site_url('site/editGallery?id=');?>" + resultrow.id + "'><i class='material-icons'>mode_edit</i></a><a class='btn btn-danger btn-xs waves-effect waves-light red pad10 z-depth-0 less-pad' onclick=\"return confirm('Are you sure you want to delete?');\" href='<?php echo site_url('site/deleteGallery?id='); ?>" + resultrow.id + "'><i class='material-icons propericon'>delete</i></a></td></tr></div><li>";
     }
     
     getDragDropOrdering("<?php echo $base_url;?>" , "<?php echo $orderfield; ?>","<?php echo $tablename;?>" ,"<?php echo $where;?>");
